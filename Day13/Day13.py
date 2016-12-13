ade34e6904a5adc0837586e97632c6985427fa9b
Marcus Stuhr <marcus.stuhr@gmail.com>
2016-12-13 10:59:20 -0500

    BFS instead of Dijkstra, both parts 1 function

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -1,5 +1,4 @@
-import heapq
-from collections import defaultdict
+from collections import deque
 
 FAVE_NUM = 1362
 MAZE_SIZE = 40
@@ -8,43 +7,31 @@ OPEN_CHAR = '.'
 IMPOSSIBLE_COST = float('inf')
 
 def f(x,y):
-    return [OPEN_CHAR, WALL_CHAR][bin(x*x + 3*x + 2*x*y + y + y*y + FAVE_NUM)[2:].count('1') % 2]
+    return [OPEN_CHAR, WALL_CHAR][bin(x*x + 3*x + 2*x*y + y + y*y + FAVE_NUM).count('1') % 2]
 
-def shortest_path(G, start, end):
-    q = [(0, start, ())]
+def BFS(initialPos, goalPos, maze, distMax = None):
+    q = deque()
+    q.append((initialPos, 0))
     visited = set()
+    posUnderDistMax = set()
     while q:
-        (cost, v1, path) = heapq.heappop(q)
-        if v1 not in visited:
-            visited.add(v1)
-            if v1 == end:
-                return cost
-            path = (v1, path)
-            for (v2, cost2) in G[v1].items():
-                if v2 not in visited:
-                    heapq.heappush(q, (cost + cost2, v2, path))
-    return IMPOSSIBLE_COST
+        curPos, dist = q.popleft()
+        if distMax == None and curPos == goalPos:
+            return (dist, set())
+        if distMax != None and dist <= distMax:
+            posUnderDistMax.add(curPos)
+        if curPos not in visited:
+            visited.add(curPos)
+            y, x = curPos
+            for deltaY, deltaX in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                newY, newX = y + deltaY, x + deltaX
+                if 0 <= newY < MAZE_SIZE and 0 <= newX < MAZE_SIZE \
+                    and maze[newY][newX] == OPEN_CHAR \
+                    and (newY, newX) not in visited:
+                        q.append(((newY, newX), dist + 1))
+    return (IMPOSSIBLE_COST, posUnderDistMax)
 
-edges = defaultdict(dict)
-distinctCoordPairs = set()
-M = [[f(x, y) for x in range(MAZE_SIZE)] for y in range(MAZE_SIZE)]
+maze = [[f(x, y) for x in range(MAZE_SIZE)] for y in range(MAZE_SIZE)]
 
-for x in range(MAZE_SIZE):
-    for y in range(MAZE_SIZE):
-        if x - 1 >= 0 and M[x - 1][y] == OPEN_CHAR:
-            edges[(x, y)][(x - 1, y)] = 1
-        if x + 1 < MAZE_SIZE and M[x + 1][y] == OPEN_CHAR:
-            edges[(x, y)][(x + 1, y)] = 1
-        if y - 1 >= 0 and M[x][y - 1] == OPEN_CHAR:
-            edges[(x, y)][(x, y - 1)] = 1
-        if y + 1 < MAZE_SIZE and M[x][y + 1] == OPEN_CHAR:
-            edges[(x, y)][(x, y + 1)] = 1
-
-for x in range(MAZE_SIZE):
-    for y in range(MAZE_SIZE):
-        d = shortest_path(edges, (1, 1), (x, y))
-        if d <= 50:
-            distinctCoordPairs.add((x, y))
-
-print(shortest_path(edges, (1, 1), (39, 31))) #part 1 answer
-print(len(distinctCoordPairs)) #part 2 answer
+print(BFS((1, 1), (39, 31), maze)[0]) #part 1 answer
+print(len(BFS((1, 1), None, maze, 50)[1])) #part 2 answer
