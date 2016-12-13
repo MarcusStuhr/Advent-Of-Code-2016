a2282be9405cdc362726ec23f6bc54c7f6319ed9
Marcus Stuhr <marcus.stuhr@gmail.com>
2016-12-13 16:42:12 -0500

    Remove max maze size

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -1,15 +1,17 @@
 from collections import deque
 
 FAVE_NUM = 1362
-MAZE_SIZE = 100
 WALL_CHAR = '#'
 OPEN_CHAR = '.'
 IMPOSSIBLE_DIST = float('inf')
 
-def f(x,y):
-    return [OPEN_CHAR, WALL_CHAR][bin(x*x + 3*x + 2*x*y + y + y*y + FAVE_NUM).count('1') % 2]
+def f(x, y, memo = {}):
+    key = (x,y)
+    if key in memo: return memo[key]
+    memo[key] = [OPEN_CHAR, WALL_CHAR][bin(x*x + 3*x + 2*x*y + y + y*y + FAVE_NUM).count('1') % 2]
+    return memo[key]
 
-def BFS(initialPos, goalPos, maze, distMax = None):
+def BFS(initialPos, goalPos, distMax = None):
     q = deque()
     q.append((initialPos, 0))
     visited = set()
@@ -25,13 +27,11 @@ def BFS(initialPos, goalPos, maze, distMax = None):
             y, x = curPos
             for deltaY, deltaX in ((0, 1), (1, 0), (0, -1), (-1, 0)):
                 newY, newX = y + deltaY, x + deltaX
-                if 0 <= newY < MAZE_SIZE and 0 <= newX < MAZE_SIZE \
-                    and maze[newY][newX] == OPEN_CHAR \
+                if 0 <= newY and 0 <= newX \
+                    and f(newX, newY) == OPEN_CHAR \
                     and (newY, newX) not in visited:
                         q.append(((newY, newX), dist + 1))
     return (IMPOSSIBLE_DIST, posWithinDistMax)
 
-maze = [[f(x, y) for x in range(MAZE_SIZE)] for y in range(MAZE_SIZE)]
-
-print(BFS((1, 1), (39, 31), maze)[0]) #part 1 answer
-print(len(BFS((1, 1), None, maze, 50)[1])) #part 2 answer
+print(BFS((1, 1), (39, 31))[0]) #part 1 answer
+print(len(BFS((1, 1), None, 50)[1])) #part 2 answer
