1241a87edd32e9c54f626a48a2f0f9a3bdd69be5
Marcus Stuhr <marcus.stuhr@gmail.com>
2016-12-14 15:44:31 -0500

    Rename hash -> cur_hash

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -6,27 +6,27 @@ import re
 SALT = "yjdafjpo"
 
 def get_hash(salt, index, num_keystretch_iterations):
-    hash = md5(str(salt + str(index)).encode()).hexdigest()
+    cur_hash = md5(str(salt + str(index)).encode()).hexdigest()
     for stretch_index in range(num_keystretch_iterations):
-        hash = md5(hash.encode()).hexdigest()
-    return hash
+        cur_hash = md5(cur_hash.encode()).hexdigest()
+    return cur_hash
 
-def resolve_k_counts(counts, k, hash, index):
+def resolve_k_counts(counts, k, cur_hash, index):
     for ch_key in "0123456789abcdef":
-        counts[index][ch_key] = counts[index - 1][ch_key] + (ch_key * k in hash)
+        counts[index][ch_key] = counts[index - 1][ch_key] + (ch_key * k in cur_hash)
 
 def find_index_one_time_pad_key(salt, target_pad_key_count, num_peek_ahead, num_reps_first, num_reps_second, num_keystretch_iterations = 0):
     pads_found = 0
     rolling_cache = deque([get_hash(salt, index, num_keystretch_iterations) for index in range(num_peek_ahead)])
     counts = defaultdict(lambda : defaultdict(int))
-    for hash_index, hash in enumerate(rolling_cache):
-        resolve_k_counts(counts, num_reps_second, hash, hash_index)
+    for hash_index, cur_hash in enumerate(rolling_cache):
+        resolve_k_counts(counts, num_reps_second, cur_hash, hash_index)
     for index in count(0):
-        hash  = rolling_cache[0]
+        cur_hash  = rolling_cache[0]
         peek_hash = get_hash(salt, index + num_peek_ahead, num_keystretch_iterations)
         rolling_cache.append(peek_hash)
         resolve_k_counts(counts, num_reps_second, peek_hash, index + num_peek_ahead)
-        cur_rep = re.search(r"(\w){}".format("\\1" * (num_reps_first - 1)), hash)
+        cur_rep = re.search(r"(\w){}".format("\\1" * (num_reps_first - 1)), cur_hash)
         if cur_rep:
             ch_key = cur_rep.group()[0]
             if counts[index + num_peek_ahead][ch_key] > counts[index][ch_key]:
