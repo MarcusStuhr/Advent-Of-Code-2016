b731d6c2dc28af155f5e432abe850acbc53b404e
Marcus Stuhr <marcusstuhr@Watson.home>
2016-12-15 03:30:14 -0500

    Wrap into function body, renaming

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,6 +1,6 @@
 import re
 
-def egcd(a, b):
+def extended_gcd(a, b):
    x, y, u, v = 0, 1, 1, 0
    while b != 0:
        q, a, b = a // b, b, a % b
@@ -8,34 +8,35 @@ def egcd(a, b):
        y, v = v - q * y, y
    return a, u, v
 
-def inverse(a, m):
-   (g, x, y) = egcd(a,m)
+def modular_inverse(a, m):
+   (g, x, y) = extended_gcd(a,m)
    if g != 1:
        raise ValueError("x (%d) and n (%d) are not coprime" % (a, m))
    return x % m
 
-def crt(vals, mods):
+def chinese_remainder_theorem(vals, mods):
    m = 1
    x = 0
    for mod in mods:
        m *= mod
    for (m_i, a_i) in zip(mods, vals):
        M_i = m // m_i
-       inv = inverse(M_i, m_i)
+       inv = modular_inverse(M_i, m_i)
        x = (x + a_i * M_i * inv) % m
    return x
 
+def get_earliest_time(lines):
+    values = []
+    moduli = []
+    for line in lines:
+        disc, num_positions, at_time, cur_pos = map(int, re.findall(r"([\d]+)", line))
+        values.append(-disc - (cur_pos - at_time) % num_positions)
+        moduli.append(num_positions)
+    return chinese_remainder_theorem(values, moduli)
+
 DATA_FILENAME = "data.txt"
 lines = open(DATA_FILENAME).read().split("\n")
-values = []
-moduli = []
 
-for line in lines:
-    disc, num_positions, at_time, cur_pos = map(int, re.findall(r"([\d]+)", line))
-    values.append(-disc - (cur_pos - at_time) % num_positions)
-    moduli.append(num_positions)
-
-print(crt(values, moduli)) #part 1 answer
-values.append(-(len(values) + 1))
-moduli.append(11)
-print(crt(values, moduli)) #part 2 answer
+print(get_earliest_time(lines)) #part 1 answer
+lines.append("Disc #{} has {} positions; at time={}, it is at position {}.".format(len(lines) + 1, 11, 0, 0))
+print(get_earliest_time(lines)) #part 2 answer
