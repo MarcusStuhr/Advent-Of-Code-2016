3bf3a004bc47360c1c4900298b982ca483756da4
Marcus Stuhr <marcusstuhr@Watson.home>
2016-12-16 00:29:25 -0500

    Add day 16

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -1,4 +1,15 @@
-import re
+def get_checksum(disk):
+    checksum = disk
+    while True:
+        checksum = ''.join(['0', '1'][checksum[i] == checksum[i + 1]] for i in range(0, len(checksum), 2))
+        if len(checksum) % 2 == 1:
+            return checksum
 
-DATA_FILENAME = "data.txt"
-lines = open(DATA_FILENAME).read().split("\n")
+def fill_disk(state, disk_size):
+    while len(state) < disk_size:
+        state = state + '0' + ''.join('1' if x == '0' else '0' for x in state[::-1])
+    return state[:disk_size]
+
+state = "00101000101111010"
+print(get_checksum(fill_disk(state, 272))) #part 1 answer
+print(get_checksum(fill_disk(state, 35651584))) #part 2 answer
