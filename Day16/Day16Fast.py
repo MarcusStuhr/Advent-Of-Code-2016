7a69579d5c21d23585a171de4f64d2b9e3d6bf81
Marcus Stuhr <marcusstuhr@Watson.local>
2016-12-17 07:21:31 -0500

    Added count memo

--- Day16/Day16Fast.py
+++ Day16/Day16Fast.py
@@ -17,25 +17,32 @@ def get_inverse_dragon_bitcount_parity(n):
     if n < 0 : return 0
     return ((n - bin(n ^ (n >> 1)).count('1')) >> 1) & 1
 
-def cumulative_bitcount_parity(pos, len_a, a, b):
+def construct_counts_raw_bitstring(a):
+    counts = [0]*(len(a) + 1)
+    for i in range(1, len(a) + 1):
+        counts[i] = counts[i-1] + (a[i - 1] == '1')
+    return counts
+
+def cumulative_bitcount_parity_expanded(pos, len_a, counts_a, counts_b):
     if pos < 0: return 0
     m = pos // (2 * len_a + 2)
     index_chunk_start = m * (2 * len_a + 2)
     offset_within_chunk = pos - index_chunk_start
     dragon_index_so_far = 2 * m - 1 + (offset_within_chunk >= len_a) + (offset_within_chunk == 2 * len_a + 2 - 1)
-    parity_a = a[:offset_within_chunk + 1].count('1') & 1
-    parity_b = b[:max(0, offset_within_chunk - len_a)].count('1') & 1
+    parity_a = counts_a[min(len_a, offset_within_chunk + 1)] & 1
+    parity_b = counts_b[min(len_a, max(0, offset_within_chunk - len_a))] & 1
     return (len_a * m + parity_a + parity_b + get_inverse_dragon_bitcount_parity(dragon_index_so_far + 1)) & 1
 
 def get_checksum(a, disk_capacity):
-    b = reverse_and_flip_bits(a)
+    counts_a, counts_b = construct_counts_raw_bitstring(a), construct_counts_raw_bitstring(reverse_and_flip_bits(a))
     x = largest_pow_2_dividing(disk_capacity)
     checksum = ""
     for k in range(disk_capacity // x):
-        count_right = cumulative_bitcount_parity((k + 1) * x - 1, len(a), a, b)
-        count_left = cumulative_bitcount_parity(k * x - 1, len(a), a, b)
+        count_right = cumulative_bitcount_parity_expanded((k + 1) * x - 1, len(a), counts_a, counts_b)
+        count_left = cumulative_bitcount_parity_expanded(k * x - 1, len(a), counts_a, counts_b)
         checksum += str(int(count_right - count_left & 1 == 0))
     return checksum if disk_capacity & 1 == 0 else flip_bits(checksum)
 
 print(get_checksum(input_code, 272))
 print(get_checksum(input_code, 35651584))
+
