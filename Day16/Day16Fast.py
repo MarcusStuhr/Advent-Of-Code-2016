e38db82c16c67f97bbaf9b29df3bdd4429c37c96
Marcus Stuhr <marcusstuhr@Watson.local>
2016-12-17 06:51:53 -0500

    Compress cumulative bitcount func

--- Day16/Day16Fast.py
+++ Day16/Day16Fast.py
@@ -17,33 +17,23 @@ def get_inverse_dragon_bitcount_parity(n):
     if n < 0 : return 0
     return ((n - bin(n ^ (n >> 1)).count('1')) >> 1) & 1
 
-def cumulative_bitcount_parity(pos, L, a, b):
+def cumulative_bitcount_parity(pos, len_a, a, b):
     if pos < 0: return 0
-    m = pos // (2 * L + 2)
-    index_chunk_start = m * (2 * L + 2)
-    parity_ab_up_to_m = L * m % 2
+    m = pos // (2 * len_a + 2)
+    index_chunk_start = m * (2 * len_a + 2)
     offset_within_chunk = pos - index_chunk_start
-    dragon_index_so_far = 2 * m - 1
-    parityA = a.count('1') % 2
-    parityB = 0
-    if 0 <= offset_within_chunk < L:
-        parityA = a[:offset_within_chunk + 1].count('1') % 2
-    if offset_within_chunk >= L:
-        dragon_index_so_far += 1
-    if offset_within_chunk == 2 * L + 2 - 1:
-        dragon_index_so_far += 1
-    if L < offset_within_chunk:
-        parityB = b[:offset_within_chunk - L].count('1') % 2
-    return (parity_ab_up_to_m + parityA + parityB + get_inverse_dragon_bitcount_parity(dragon_index_so_far + 1)) % 2
+    dragon_index_so_far = 2 * m - 1 + (offset_within_chunk >= len_a) + (offset_within_chunk == 2 * len_a + 2 - 1)
+    parity_a = a[:offset_within_chunk + 1].count('1') % 2
+    parity_b = b[:max(0, offset_within_chunk - len_a)].count('1') % 2
+    return (len_a * m + parity_a + parity_b + get_inverse_dragon_bitcount_parity(dragon_index_so_far + 1)) % 2
 
 def get_checksum(a, disk_capacity):
     b = reverse_and_flip_bits(a)
-    L = len(a)
     x = largest_pow_2_dividing(disk_capacity)
     checksum = ""
     for k in range(disk_capacity//x):
-        count_right = cumulative_bitcount_parity((k + 1) * x - 1, L, a, b)
-        count_left = cumulative_bitcount_parity(k * x - 1, L, a, b)
+        count_right = cumulative_bitcount_parity((k + 1) * x - 1, len(a), a, b)
+        count_left = cumulative_bitcount_parity(k * x - 1, len(a), a, b)
         checksum += str(int(count_right - count_left % 2 == 0))
     return checksum if disk_capacity % 2 == 0 else flip_bits(checksum)
 
