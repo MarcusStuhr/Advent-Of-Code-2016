1ab72ba616319ecb39341d61d93bc699c9780c59
Marcus Stuhr <MarcusStuhr@users.noreply.github.com>
2016-12-20 11:23:14 -0500

    Update Day20.py

--- Day20/Day20.py
+++ Day20/Day20.py
@@ -6,14 +6,14 @@ ips.append((MAX_IP + 1, MAX_IP + 2))
 
 count_allowed = 0
 max_hi_ip_so_far = MIN_IP - 1
-lowest_allowed = None
+lowest_ip_allowed = None
 
 for lo_ip, hi_ip in ips:
     if lo_ip > max_hi_ip_so_far + 1:
-        if lowest_allowed == None:
-            lowest_allowed = max_hi_ip_so_far + 1
+        if lowest_ip_allowed == None:
+            lowest_ip_allowed = max_hi_ip_so_far + 1
         count_allowed += lo_ip - max_hi_ip_so_far - 1
     max_hi_ip_so_far = max(max_hi_ip_so_far, hi_ip)
 
-print(lowest_allowed)
+print(lowest_ip_allowed)
 print(count_allowed)
