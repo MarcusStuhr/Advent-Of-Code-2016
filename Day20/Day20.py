cf262ec5cfb819c313877c3358a8b7a4e2def9bd
Marcus Stuhr <MarcusStuhr@users.noreply.github.com>
2016-12-20 11:16:09 -0500

    Update Day20.py

--- Day20/Day20.py
+++ Day20/Day20.py
@@ -1,18 +1,19 @@
-DATA_FILENAME = "data.txt"
+MIN_IP = 0
 MAX_IP = 2**32 - 1
-ips = sorted([tuple(map(int, line.split('-'))) for line in open(DATA_FILENAME).read().split("\n")])
-ips.append(tuple([MAX_IP + 1, MAX_IP + 2])) #dummy item for upper bounding
+lines = open("data.txt").read().split("\n")
+ips = sorted(tuple(map(int, line.split('-'))) for line in lines if '-' in line)
+ips.append((MAX_IP + 1, MAX_IP + 2))
 
 count_allowed = 0
-prev_highest = -1
+max_hi_ip_so_far = MIN_IP - 1
 lowest_allowed = None
 
 for lo_ip, hi_ip in ips:
-    if lo_ip > prev_highest + 1:
+    if lo_ip > max_hi_ip_so_far + 1:
         if lowest_allowed == None:
-            lowest_allowed = lo_ip - 1
-        count_allowed += lo_ip - prev_highest - 1
-    prev_highest = max(prev_highest, hi_ip)
+            lowest_allowed = max_hi_ip_so_far + 1
+        count_allowed += lo_ip - max_hi_ip_so_far - 1
+    max_hi_ip_so_far = max(max_hi_ip_so_far, hi_ip)
 
-print(lowest_allowed if ips[0][0] == 0 else 0)
+print(lowest_allowed)
 print(count_allowed)
