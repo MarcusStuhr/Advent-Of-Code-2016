3c79c3c782e40f71da8956a20799f28f45427edf
Marcus Stuhr <marcusstuhr@Watson.home>
2016-12-21 02:08:40 -0500

    Add day 21

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -1,4 +1,56 @@
 import re
+from itertools import permutations
 
-DATA_FILENAME = "data.txt"
-lines = open(DATA_FILENAME).read().split("\n")
+def rotate(code, steps, towards_right = True):
+    slice_sign = -1 if towards_right else 1
+    steps %= len(code)
+    return code[slice_sign * steps:] + code[:slice_sign * steps]
+
+def scrambler(input_string, instructions):
+    code = list(input_string)
+
+    for line in instructions:
+        nums = re.findall(r"([\d]+)", line)
+        letters = re.findall(r"letter ([\w]+)", line)
+
+        if line.startswith("swap"):
+            if len(letters) > 0:
+                pos1, pos2 = code.index(letters[0]), code.index(letters[1])
+            else:
+                pos1, pos2 = map(int, nums)
+            code[pos1], code[pos2] = code[pos2], code[pos1]
+
+        elif line.startswith("rotate left"):
+            code = rotate(code, int(nums[0]), False)
+
+        elif line.startswith("rotate right"):
+            code = rotate(code, int(nums[0]), True)
+
+        elif line.startswith("rotate based"):
+            pos = code.index(letters[0])
+            if pos >= 4:
+                pos += 1
+            code = rotate(code, pos + 1, True)
+
+        elif line.startswith("move"):
+            from_pos, to_pos = map(int, nums)
+            letter_to_insert = code[from_pos]
+            code.remove(letter_to_insert)
+            code.insert(to_pos, letter_to_insert)
+
+        elif line.startswith("reverse"):
+            startPos, endPos = map(int, nums)
+            code = code[:startPos] + code[startPos:endPos + 1][::-1] + code[endPos + 1:]
+
+    return ''.join(code)
+
+
+instructions = open("data.txt").read().split("\n")
+
+print(scrambler("abcdefgh", instructions)) #part 1 answer
+
+for p in permutations("abcdefgh"):
+    input_string = ''.join(p)
+    if scrambler(input_string, instructions) == "fbgdceah":
+        print(input_string) #part 2 answer
+        break
