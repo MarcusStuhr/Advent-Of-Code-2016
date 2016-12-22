35d14e27a9ef0256aab682ed830a386ca06f25b6
Marcus Stuhr <marcusstuhr@Watson.home>
2016-12-21 20:01:00 -0500

    Fix spacing

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -3,4 +3,3 @@ from Scrambler import scrambler
 instructions = open("data.txt").read().split("\n")
 print(scrambler("abcdefgh", instructions)) #part 1 answer
 print(scrambler("fbgdceah", instructions, True)) #part 2 answer
-
