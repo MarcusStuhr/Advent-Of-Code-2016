c414ec560473fc271f5771180ffbc7748ecde937
Marcus Stuhr <marcusstuhr@Watson.home>
2016-12-23 01:42:34 -0500

    Update day 12, push day 23

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -1,34 +1,38 @@
 from collections import defaultdict
 
 DATA_FILENAME = "data.txt"
-lines = open(DATA_FILENAME).read().split("\n")
 
-for defaultC in (0, 1):
+def resolve_value(char, registers):
+    return int(registers[char]) if char.isalpha() else int(char)
+
+for default_c in (0, 1):
+    lines = open(DATA_FILENAME).read().split("\n")
     registers = defaultdict(int)
-    registers["c"] = defaultC
+    registers["c"] = default_c
     i = 0
+
     while i < len(lines):
         commands = lines[i].split(" ")
+
         if len(commands) == 2:
-            direction, x = commands
-            if direction == "inc":
+            command, x = commands
+
+            if command == "inc":
                 registers[x] += 1
-            else:
+
+            elif command == "dec":
                 registers[x] -= 1
+
         elif len(commands) == 3:
-            if commands[0] == "cpy":
-                cpy, x, y = commands
-                if (x.isalpha()):
-                    registers[y] = int(registers[x])
-                else:
-                    registers[y] = int(x)
-            else:
-                jnz, x, y = commands
-                if (not x.isalpha() and x != 0) or (x.isalpha() and registers[x] != 0):
-                    if y.isalpha():
-                        i += registers[y]
-                    else:
-                        i += int(y)
-                    i -= 1
+            command, x, y = commands
+
+            if command == "cpy":
+                registers[y] = resolve_value(x, registers)
+
+            elif command == "jnz":
+                if resolve_value(x, registers) != 0:
+                    i += resolve_value(y, registers) - 1
+
         i += 1
+
     print(registers['a']) #part 1 and 2 answer
--- Day23/Day23.py
+++ Day23/Day23.py
@@ -1,4 +1,62 @@
-import re
+from collections import defaultdict
 
-DATA_FILENAME = "data.txt"
-lines = open(DATA_FILENAME).read().split("\n")
+DATA_FILENAME = "data.txt" #this data has been altered to use the mul / nop codes
+
+def resolve_value(char, registers):
+    return int(registers[char]) if char.isalpha() else int(char)
+
+def toggle_line(line):
+    swaps = {"tgl":"inc", "dec":"inc", "inc":"dec", "cpy":"jnz", "jnz":"cpy"}
+    split_line = line.split(" ")
+    split_line[0] = swaps[split_line[0]]
+    return ' '.join(split_line)
+
+for default_a in (7, 12):
+    lines = open(DATA_FILENAME).read().split("\n")
+    registers = defaultdict(int)
+    registers["a"] = default_a
+    i = 0
+
+    while i < len(lines):
+        commands = lines[i].split(" ")
+
+        if len(commands) == 1:
+            command = commands
+
+            if command == "nop":
+                pass
+
+        elif len(commands) == 2:
+            command, x = commands
+
+            if command == "inc":
+                registers[x] += 1
+
+            elif command == "dec":
+                registers[x] -= 1
+
+            elif command == "tgl":
+                target_index = i + resolve_value(x, registers)
+                if 0 <= target_index < len(lines):
+                    lines[target_index] = toggle_line(lines[target_index])
+
+        elif len(commands) == 3:
+            command, x, y = commands
+
+            if command == "cpy":
+                registers[y] = resolve_value(x, registers)
+
+            elif command == "jnz":
+                if resolve_value(x, registers) != 0:
+                    i += resolve_value(y, registers) - 1
+
+        elif len(commands) == 4:
+            command, x, y, z = commands
+
+            if command == "mul":
+                x, y = resolve_value(x, registers), resolve_value(y, registers)
+                registers[z] = x * y
+
+        i += 1
+
+    print(registers['a']) #part 1 and 2 answer
